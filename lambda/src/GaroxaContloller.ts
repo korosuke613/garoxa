ae00feb50043dc11541175ca2e25e9576766b2a0
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2020-10-21 17:10:55 +0900

    wip

--- lambda/src/GaroxaContloller.ts
+++ lambda/src/GaroxaContloller.ts
@@ -6,15 +6,31 @@ export interface GaroonScheduleDetail{
     date: Date
 }
 
+export interface GaroonLoginInformation{
+    username: string
+    password: string
+    domain: string
+    timezone: string
+}
+
 export class GaroxaContloller{
     private readonly client: GaroonRestAPIClient
+    private readonly garoonLoginInformation: GaroonLoginInformation
+
     constructor() {
+        this.garoonLoginInformation = {
+            username: process.env.GAROON_USERNAME,
+            password: process.env.GAROON_PASSWORD,
+            domain: process.env.GAROON_DOMAIN,
+            timezone: process.env.GAROON_TIMEZONE || 'Asia/Tokyo'
+        }
+
         this.client = new GaroonRestAPIClient({
-            baseUrl: `https://${process.env.GAROON_DOMAIN}.cybozu.com/g`,
+            baseUrl: `https://${this.garoonLoginInformation.domain}.cybozu.com/g`,
             // Use password authentication
             auth: {
-                username: process.env.GAROON_USERNAME,
-                password: process.env.GAROON_PASSWORD,
+                username: this.garoonLoginInformation.username,
+                password: this.garoonLoginInformation.password,
             },
             // Use OAuth token authentication
             // auth: { oAuthToken: process.env.GAROON_OAUTH_TOKEN }
@@ -28,16 +44,16 @@ export class GaroxaContloller{
             eventType: "ALL_DAY",
             start: {
                 dateTime: "2020-10-21T00:00:00+09:00",
-                timeZone: "Asia/Tokyo"
+                timeZone: this.garoonLoginInformation.timezone
             },
             end: {
                 dateTime: "2020-10-22T23:59:59+09:00",
-                timeZone: "Asia/Tokyo"
+                timeZone: this.garoonLoginInformation.timezone
             },
             attendees: [
                 {
                     type: "USER",
-                    code: "sato"
+                    code: this.garoonLoginInformation.username
                 }
             ],
         })
